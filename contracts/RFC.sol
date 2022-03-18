72b0a6ef0284149c1de606ae04f5454c9a2aaea9
ManishGotame <manishgt194@gmail.com>
2022-03-18 20:21:26 +0545

    ERC721URIStorage integration

--- contracts/RFC.sol
+++ contracts/RFC.sol
@@ -1,4 +1,3 @@
-
 // SPDX-License-Identifier: UNLICENSED
 
 pragma solidity ^0.8.7;
@@ -7,13 +6,15 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";  
-
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "hardhat/console.sol";
+
+// need to pull all the contract to access the private variables.
 
 // TODO: need to add the URIStorage here
 
-contract ResearchFundingClub is ERC721Enumerable, Ownable {
+contract ResearchFundingClub is ERC721Enumerable, Ownable, ERC721URIStorage {
     string public baseTokenURI;
 
     bool public paused = false;
@@ -24,15 +25,58 @@ contract ResearchFundingClub is ERC721Enumerable, Ownable {
     uint256 public MAX_PER_MINT = 1;
     mapping(address => uint256) public addressMintedBalance;
 
-    // testing
-
-    uint256 public prevRandomNumber = 1; 
-
     constructor(string memory baseURI) ERC721("Research Funding Club", "RFC") {
         setBaseURI(baseURI);
     }
+    
+    // ERC721URIStorage override functions 
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
 
-    function _baseURI() internal view virtual override returns (string memory) {
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    // function tokenURI(uint256 tokenId)
+    //     public
+    //     view
+    //     override(ERC721, ERC721URIStorage)
+    //     returns (string memory)
+    // {
+    //     return super.tokenURI(tokenId);
+    //     // string memory tokenUri = super.tokenURI(tokenId);
+    //     // return abi.encodePacked(tokenUri, baseExtension);
+    //     // return ERC721URIStorage._tokenURIs[tokenId];
+    // }
+
+    function tokenURI(uint256 tokenId)
+        public view virtual override returns (string memory)
+    {
+        require(
+            _exists(tokenId),
+            "ERC721URIStorage: URI query for nonexistent token"
+        );
+    return _tokenURIs[tokenId];
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+    // function _baseURI() internal view virtual override returns (string memory) {
+    //     return baseTokenURI;
+    // }
+
+    function _baseURI() internal view override returns (string memory) {
         return baseTokenURI;
     }
 
@@ -81,35 +125,32 @@ contract ResearchFundingClub is ERC721Enumerable, Ownable {
         @returns <uint> - unminted random number
     */
 
-    // cleaner
-    // function selectRandomNumber(uint256 randomNo) internal view returns (uint256) {
-    //     uint256 supply = totalSupply();
-    //     require(supply < MAX_SUPPLY, "Ran out of numbers.");
-
-    //     uint256 mintNumber = rand(msg.sender, randomNo);        
-    //     if (ERC721.testOwnerOf(mintNumber) == false) return mintNumber;
-    //     return selectRandomNumber(mintNumber + randomNo);
-    // }
-
     function selectRandomNumber(uint256 randomNo) internal view returns (uint256) {
         uint256 supply = totalSupply();
-        require(supply < MAX_SUPPLY, "no numbers left.");
+        require(supply < MAX_SUPPLY, "Ran out of numbers.");
 
-        uint256 mintNumber = rand(msg.sender, randomNo);
-        
-        console.log("random number: ", mintNumber);
-
-        // if (ERC721.testOwnerOf(mintNumber) == false) return mintNumber;
-        if (ERC721.testOwnerOf(mintNumber) == false) {
-            console.log("Found final mint number: ", mintNumber);
-            return mintNumber;
-        }
-
-        console.log("Oops mint number was duplicate.");
+        uint256 mintNumber = rand(msg.sender, randomNo);        
+        if (ERC721.testOwnerOf(mintNumber) == false) return mintNumber;
         return selectRandomNumber(mintNumber + randomNo);
     }
 
-    function mint(uint256 _mintAmount, uint256 mintNumber) public payable {
+    // function selectRandomNumber(uint256 randomNo) internal view returns (uint256) {
+    //     uint256 supply = totalSupply();
+    //     require(supply < MAX_SUPPLY, "no numbers left.");
+
+    //     uint256 mintNumber = rand(msg.sender, randomNo);
+    //     console.log("random number: ", mintNumber);
+
+    //     if (ERC721.testOwnerOf(mintNumber) == false) {
+    //         console.log("Found final mint number: ", mintNumber);
+    //         return mintNumber;
+    //     }
+
+    //     console.log("Oops mint number was duplicate.");
+    //     return selectRandomNumber(mintNumber + randomNo);
+    // }
+
+    function mint(uint256 _mintAmount) public payable {
         require(!paused, "The contract is paused");
         uint256 supply = totalSupply();
         require(_mintAmount > 0, "mint atleast 1 NFT");
@@ -123,39 +164,19 @@ contract ResearchFundingClub is ERC721Enumerable, Ownable {
         // uint256 ownerMintedCount = addressMintedBalance[msg.sender];
         // require(ownerMintedCount + _mintAmount <= MAX_PER_MINT, "max NFT per address exceeded");
 
+        uint256 mintNumber = selectRandomNumber(0);
         require(msg.value >= PRICE * _mintAmount, "insufficient funds");  
         
         addressMintedBalance[msg.sender]++;
         _safeMint(msg.sender, mintNumber);
+
+        string memory tokenUri = Strings.toString(mintNumber);
+        // _setTokenURI(mintNumber, abi.encodePacked(tokenUri, baseExtension));
+        _setTokenURI(mintNumber, tokenUri);
     }
 
     function generateRandomNumber() public view returns (uint256) {
         return selectRandomNumber(0);
-
-        // console.log(ERC721.testOwnerOf(1));
-        // if (ownerOf(1)) {
-        //     console.log("found");
-        // } else {
-        //     console.log("not found");
-        // }
-        
-        // try ownerOf(1) {
-        //     console.log(ownerOf(1));
-        // } catch Error(string memory /*reason */) {
-        //     console.log("no owners");
-        // }
-
-        // console.log(ownerOf(1));
-        // console.log(ERC721._owners[0]);
-
-
-        // uint256 no = selectRandomNumber(1);
-        // console.log(no);
-        // prevRandomNumber = rand(msg.sender, prevRandomNumber);
-    }
-
-    function currentRandomNumber() public view returns (uint256) {
-        return prevRandomNumber;
     }
 
     function setmaxMintAmount(uint256 _limit) public onlyOwner {
@@ -178,10 +199,6 @@ contract ResearchFundingClub is ERC721Enumerable, Ownable {
         baseExtension = _newBaseExtension;
     }
 
-    // function setNotRevealedURI(string memory _notRevealedURI) public onlyOwner {
-    //     notRevealedUri = _notRevealedURI;
-    // }
-
     function pause(bool _state) public onlyOwner {
         paused = _state;
     }
@@ -194,3 +211,7 @@ contract ResearchFundingClub is ERC721Enumerable, Ownable {
         require(success, "Transfer failed.");
     }
 }
+
+/*
+    Developed by Manish Gotame - https://manishgotame.com.np/
+ */
